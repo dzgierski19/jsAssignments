95f6e621dd0630676667b634fbc052ec01ac1f0d
Zgierski Dawid <zgiersd@omnis.netia.org>
2022-12-31 12:53:29 +0100

    christmasTree validation added

--- christmasTree.js
+++ christmasTree.js
@@ -10,9 +10,58 @@ for (let i=1; i<treeHeight; i++) {
 
 //christmasTree jest metoda ktora pozwala powielić spację
 
-const showChristmasTree = (treeHeight) => {
+//WALIDACJA
 
+const treeHeightSection = (treeHeight) => {
+    if (!(1<=treeHeight<=10)) {
+        throw new Error ("Please type number from 1 to 10")
+    }
+}
+
+const treeHeightIsNumber = (treeHeight) => {
+    if (typeof treeHeight === "number" && !isNaN(treeHeight)){
+        return true
+    } else { 
+        throw new Error ("Must be a number")
+    }
+}
+
+const treeHeightIsInteger = (treeHeight) => {
+    if (!(Number.isInteger(treeHeight))) {
+        throw new Error ("Must be an integer")
+    } 
+}
+
+const space = ' '
+
+const topLineOfChristmasTree = (space.repeat(4) + '*') 
+console.log(topLineOfChristmasTree)
+
+const bottomLineOfChristmasTree = (space.repeat(4) + '|')
+console.log(bottomLineOfChristmasTree)
+
+let star2 = '*'
+let doubleStar = (star2.repeat())
+console.log(doubleStar)
+
+const showChristmasTree = (treeHeight) => {
+treeHeightIsNumber(treeHeight)
+treeHeightSection(treeHeight)
+treeHeightIsInteger(treeHeight)
+const tree = []
+    for (let i = 0; i<treeHeight; i++) {
+    const star2 = '*'
+    const space = ' '
+    let modifiedStar = star2.repeat(i)
+    console.log(modifiedStar)
+    tree.push(modifiedStar)
+    i++
+} return tree
 } 
+const myResult1 = console.log(showChristmasTree(4))
+console.log (myResult1)
+
+console.log(showChristmasTree(4.5))
 
 showChristmasTree(4);
 // W konsoli powinno wyświetlić się:
@@ -26,18 +75,4 @@ showChristmasTree(4);
 
 //index * i | powiązany z treeHeight
 
-const skipIsNumber = (skip) => {
-    if (typeof skip === "number" && !isNaN(skip)){
-        return true
-    } else { 
-        throw new Error ("Must be a number")
-    }
-}
 
-const sentenceIsString = (sentence) => {
-    if (typeof sentence === "string"){
-        return true
-    } else { 
-        throw new Error ("Must be a string")
-    }
-}
