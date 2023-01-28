1367e39b0e860dd6bacce252c9baa4f849a860ff
Zgierski Dawid <zgiersd@omnis.netia.org>
2023-01-29 00:16:06 +0100

    getCleanedZipCode another method

--- getCleanedZipCode.js
+++ getCleanedZipCode.js
@@ -20,23 +20,27 @@ isInputStringOrNumber = (input) => {
         throw new Error ('You must type Number or String')}
 }
 
-console.log (isInputStringOrNumber(12))
-
+// console.log (isInputStringOrNumber(12))
 
 isInputWithNumbersSpacesAndDashes = (input) => {
-    if (input === input.replace(/[^0-9, -]/g, '')) {
+    // console.log (input.length)
+    // console.log (input[2].replace(/[^ -]/g, ''))
+    if (input.length === 6 && input[2] === input[2].replace(/[^ -]/g, ''))  {
         return input
-    } return ""
+    } else if (input.length === 5 && input === input.replace (/[^0-9]/g, '')) {
+        return input
+    } else return ""
 }
 
-const getCleanedZipCode = (Value) => {
-const checkedValue = isInputStringOrNumber(Value)
+const getCleanedZipCode = (value) => {
+const checkedValue = isInputStringOrNumber(value)
 const checkedValueWithNumbersAndSpacesAndDashes = isInputWithNumbersSpacesAndDashes (checkedValue)
+console.log (checkedValueWithNumbersAndSpacesAndDashes)
 const checkedValueWithAnythingButNumbers = checkedValueWithNumbersAndSpacesAndDashes.replace (/[^0-9]/g, '')
 if (checkedValueWithAnythingButNumbers.length !== 5) {
     return ""
 } return checkedValueWithAnythingButNumbers.slice (0,2) + '-' + checkedValueWithAnythingButNumbers.slice(2) 
 }
 
-const result1 = getCleanedZipCode('1 2  3-23')
+const result1 = getCleanedZipCode("12 223")
 console.log(result1)
