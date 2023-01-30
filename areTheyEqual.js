743d4255aa7d77cc1a4dac7645fe7717ba722c1a
Zgierski Dawid <zgiersd@omnis.netia.org>
2023-01-30 01:01:57 +0100

    Objects.value() added

--- areTheyEqual.js
+++ areTheyEqual.js
@@ -12,6 +12,8 @@ if (input1.length !== input2.length) {
 }
 }
 
+
+
 const isInputAnObject = (input1, input2) => {
     if (typeof input1 !== "object" && typeof input2 !== "object") {
         throw new Error ("It's not an object")
@@ -20,24 +22,29 @@ const isInputAnObject = (input1, input2) => {
 
 const isObjectTheSame = (input1, input2) => {
     isInputAnObject (input1, input2)
-    areObjectsTheSameLength (input1, input2)
-    if (input1.length === 0 && input2.length === 0) {
+    let input3 = Object.values(input1)
+    let input4 = Object.values(input2)
+    areObjectsTheSameLength (input3, input4)
+    if (input3.length === 0 && input4.length === 0) {
         return true
     } else {
-        let input3 = ""
-        let input4 = "" 
-        if (input1[input1.length-1] === input2[input1.length-1]){
-        input3 += input1.pop()
-        input4 += input2.pop()
+        let input5 = ""
+        let input6 = "" 
+        if (input3[input3.length-1] === input4[input4.length-1]){
+        input5 += input3.pop()
+        input6 += input4.pop()
         } else return false
-} return isObjectTheSame (input1,input2)
+} return isObjectTheSame (input3,input4)
 }
 
 const areTheyEqual = (firstValue, secondValue) => {
- return areInputsTheSameTypeAndAreNotObjects (firstValue, secondValue)
+return areInputsTheSameTypeAndAreNotObjects (firstValue, secondValue)
 }
 
-const result1 = areTheyEqual ([1,2], [1,2,3])
+let person = {name: "John", surname: "John"}
+let person2 = {name: "John", surname: "John"}
+
+const result1 = areTheyEqual (person2, person)
 console.log (result1)
 
 // console.log (areTheyEqual("8", "8")) // true
